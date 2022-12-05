f61e2180d9da6043868c350cf3c11c1d541239b6
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-12-04 22:39:39 -0800

    server exercise

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("Hello World!\n==========\n");
+
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+
+const http = require("http");
+http.createServer(function (request,response){
+    let {url} = request;
+    let chunks = [];
+    request.on("data", function(chunk){
+        chunks.push(chunk);
+        request.on("end", function() {
+            const body = Buffer.concat(chunks).toString();
+            const wildcard = {
+                make: "Aston Martin",
+                model: "V12 Speedster",
+              };
+
+              const about = {
+                name: "Elizabeth",
+                favColor: "mermaid aqua",
+              };
+
+
+            if (url === "/") {
+                response.write(JSON.stringify(wildcard));
+            } else if (url === "/about") {
+                response.write(JSON.stringify(about));
+            } else if (url === "/echo") {
+                response.write(body);
+            }
+         });
+    })
+
+})
+.listen(3000, function () {
+    console.log("Server listening on port 3000...");
+  });
